fadc78d114f6f50b3b96ea3cf998dcc28321b2f9
David Guy Brizan <dbrizan@gmail.com>
2018-10-18 22:17:22 -0700

    Initial commit

---
+++ List.java
@@ -0,0 +1,10 @@
+
+public interface List {
+	
+	public void add(Object obj);
+	public void add(int pos, Object obj);
+	public Object get(int pos);
+	public Object remove(int pos);
+	public int size();
+
+}
